87ad69c8f4db3d91f1291c2906e61f78334cdc80
bchamarthi <brchamarthi29@gmail.com>
2018-08-11 23:05:10 +0000

    Done.

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,13 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
-  get '/' do
-    erb :index
-  end
-
+  get '/' do 
+    "This is your home page" 
+  end 
+  
+  get '/itslit' do
+    erb:index 
+  end 
 end
